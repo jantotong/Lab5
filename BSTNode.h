99f236397518a3538e062a96edd0b3fc8c00db64
jantotong <43649573+jantotong@users.noreply.github.com>
2018-11-21 12:00:24 -0800

    Update BSTNode.h

--- BSTNode.h
+++ BSTNode.h
@@ -1,1 +1,36 @@
+#ifndef BSTNode_H
+#define BSTNode_H
 
+template <typename T>
+class Tree;
+
+template <typename T>
+class TreeNode
+{
+	friend class Tree<T>;
+
+public:
+	TreeNode(T);
+	T getData(); //returns data stored in node
+
+private:
+	T data;
+	TreeNode* leftPtr; //pointer to left child node of node
+	TreeNode* rightPtr; //pointer to right child node of node
+};
+
+template <typename T>
+TreeNode<T>::TreeNode(T dataIn)
+{
+	data = dataIn;
+	leftPtr = 0; //pointer to left and right child nodes are initilized to NULL
+	rightPtr = 0;
+}
+
+template <typename T>
+T TreeNode<T>::getData()
+{
+	return data;
+}
+
+#endif
