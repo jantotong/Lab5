d88f3ef9747ea9857161cd66c6563662201be6e5
jantotong <43649573+jantotong@users.noreply.github.com>
2018-11-24 14:09:25 -0800

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,15 +1,57 @@
 #define BST_H
+#ifndef NODE_H
+
 #include <iostream>
 using namespace std;
-#include "BSTNode.h"
-#include "LinkedList.h"
+#include "Node.h"
 
 template <typename T>
 class BST
 {
-public:
-	BST();
-
 private:
-	BSTNode<T> *rootPtr;
+
+public:
+
+	BSTNode<T>* root;
+
+	BST() {
+		root = NULL;
+	}
+
+	~BST() {
+		root = root->empty(root);
+	}
+
+	void insert(T x) {
+		root = root->insert(x, root);
+	}
+
+	void remove(T x) {
+		root = remove(x, root);
+	}
+
+	void display() {
+		inorder(root);
+		cout << endl;
+	}
+
+	BSTNode<T>* printPreorder() {
+		root->preorder(root);
+		cout << endl;
+		return root;
+	}
+
+	BSTNode<T>* printPostorder() {
+		root->postorder(root);
+		cout << endl;
+		return root;
+	}
+
+	BSTNode<T>* printInorder() {
+		root->inorder(root);
+		cout << endl;
+		return root;
+	}
+
 };
+#endif
